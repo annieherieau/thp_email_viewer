40ac15e04ed2907f49a31d31966a0107ad286734
annieherieau <annie.herieau@gmail.com>
2024-03-06 11:43:05 +0100

    email CRUD ok

---
+++ app/controllers/emails_controller.rb
@@ -0,0 +1,55 @@
+class EmailsController < ApplicationController
+  before_action :set_email, only: %i[ show edit update destroy ]
+  # GET /emails
+  def index
+    @emails = Email.all
+  end
+
+  # GET /emails/1
+  def show
+  end
+
+  # GET /emails/new
+  def new
+    @email = Email.new
+  end
+
+  # GET /emails/1/edit
+  def edit
+  end
+
+  # GET /emails
+  def create
+    @email = Email.new(email_params)
+    if @email.save
+      redirect_to email_url(@email), notice: "Email was successfully created."
+    else
+      # TODO render + flash
+    end
+
+  end
+
+  def update
+    if @email.update(email_params)
+      redirect_to email_url(@email), notice: "Email was successfully updated." 
+    else
+      #  ??
+    end
+  end
+
+  def destroy
+    @email.destroy!
+    redirect_to emails_url, notice: "Email was successfully destroyed." 
+  end
+
+  private
+   # Use callbacks to share common setup or constraints between actions.
+   def set_email
+    @email = Email.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def email_params
+    params.require(:email).permit(:object, :body)
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,28 @@
+# This file should ensure the existence of records required to run the application in every environment (production,
+# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Example:
+#
+#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
+#     MovieGenre.find_or_create_by!(name: genre_name)
+#   end
+
+require 'faker'
+Faker::Config.locale='fr'
+Faker::UniqueGenerator.clear
+
+Email.destroy_all
+ActiveRecord::Base.connection.tables.each do |t|
+	ActiveRecord::Base.connection.reset_pk_sequence!(t)
+end
+
+
+10.times do |i|
+  Email.create!(
+    object: Faker::Lorem.sentence,
+    body: Faker::Lorem.paragraph(sentence_count: rand(3..10))
+  )
+end
+
+
